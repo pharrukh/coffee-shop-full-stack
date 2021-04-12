83de51f47030e0fa1fa046d1a43ed3362e064e3c
pharrukh <farrukh.normuradov@gmail.com>
2021-04-13 00:06:02 +0200

    feat: implement auth

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify, abort, render_template
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
@@ -17,7 +18,7 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
 '''
@@ -29,6 +30,11 @@ CORS(app)
         or appropriate status code indicating reason for failure
 '''
 
+@app.route('/drinks', methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_short():
+    return jsonify({'success': True, 'drinks': [drink.short() for drink in Drink.query.all()]})
+
 
 '''
 @TODO implement endpoint
@@ -38,6 +44,11 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
+@app.route('/drinks-detail', methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_detail():
+    return jsonify({'success': True, 'drinks': get_drinks_long()})
 
 
 '''
@@ -49,6 +60,18 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks/', methods=['POST'])
+@requires_auth('post:drinks')
+def create_drink():
+    drink = request.get_json()
+
+    new_drink = Drink()
+    new_drink = Drink(title=drink['title'], recipe=json.dumps(drink['recipe']))
+    new_drink.insert()
+
+    return jsonify({'success': True, 'drinks': get_drinks_long()})
 
 
 '''
@@ -64,6 +87,26 @@ CORS(app)
 '''
 
 
+@app.route('/drinks/<int:id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def update_drink(id):
+    drink = Drink.query.get(id)
+    if not drink:
+        abort(404)
+
+    update_data = request.get_json()
+
+    if 'title' in update_data:
+        drink.title = update_data['title']
+
+    if 'recipe' in update_data:
+        drink.recipe = update_data['recipe']
+
+    drink.update()
+
+    return jsonify({'success': True, 'drinks': get_drinks_long()})
+
+
 '''
 @TODO implement endpoint
     DELETE /drinks/<id>
@@ -74,6 +117,16 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(id):
+    drink = Drink.query.get(id)
+    if not drink:
+        abort(404)
+    drink.delete()
+    return jsonify({'success': True, 'delete': id})
 
 
 # Error Handling
@@ -108,7 +161,51 @@ def unprocessable(error):
 '''
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(error):
+    # TODO: this can be improved
+    if error.status_code == 401:
+        return render_template('errors/401.html'), 401
+    if error.status_code == 403:
+        return render_template('errors/403.html'), 403
+    return render_template('errors/500.html'), 500
+
+
+@app.errorhandler(401)
+def server_error(error):
+    return render_template('errors/401.html'), 401
+
+
+@app.errorhandler(403)
+def server_error(error):
+    return render_template('errors/403.html'), 403
+
+
+@app.errorhandler(422)
+def server_error(error):
+    return render_template('errors/422.html'), 422
+
+
+@app.errorhandler(405)
+def server_error(error):
+    return render_template('./errors/405.html'), 405
+
+
+@app.errorhandler(409)
+def server_error(error):
+    return render_template('./errors/405.html'), 409
+
+
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+
+
+def get_drinks_long():
+    return [drink.long() for drink in Drink.query.all()]
